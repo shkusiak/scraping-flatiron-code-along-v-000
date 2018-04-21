25408b85fb5434247f0bd06ba3b3e75e97f7eb4f
Shea Kusiak <shkusiak@gmail.com>
2018-04-21 20:56:26 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,6 +2,12 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
+  def initialize
+    @title = title
+    @schedule = schedule
+    @description = description
+  end
+
   def self.all
     @@all
   end
